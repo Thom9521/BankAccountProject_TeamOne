ae31d8d8017f562517857e708385ea08edfeaf85
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-11-29 14:44:23 +0100

    Tilføjet alt muligt  lækkert

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
         System.out.println("Nice job Team One!");
 
-        Login_GUI run = new Login_GUI();
+       // Login_GUI run = new Login_GUI();
 
         Bruger.insertBrugerData();
 
