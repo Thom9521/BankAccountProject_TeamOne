024ff32f5cec7a04c53f87a7049e0410f46ac492
mich282q <31951247+mich282q@users.noreply.github.com>
2017-11-30 09:33:23 +0100

    login

--- src/Bruger.java
+++ src/Bruger.java
@@ -2,19 +2,29 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+/**
+ *Bruger klassen bliver brugt til og oprettet bruger ned i Databasen
+ * */
 
 public class Bruger {
-
+/**
+ * Søger for vores connect til vores DB fra DB_Connector klassen*/
     static Connection con = DB_Connector.connect();
     static Statement stmt = null;
 
+    /** Vi erklær vores info om vores bruger vi vil oprettet
+     * hvor vi vælger hvilken datatyper det skal være.
+     **/
     // Laver en bruger med person_id, fornavn, efternavn og adresse
     public static void lavBruger(int Person_id, String fnavn, String lnavn, String adresse) throws SQLException {
-        // Initialiserer varibler der skal bruges
+        /** // Initialiserer varibler der skal bruges
+         */
         String sql;
         int hentetId = 0;
 
-        // Henter et Person_id fra bruger tabelen hvor Person_id er Person_id variablen
+        /**Henter et Person_id fra bruger tabelen hvor Person_id er Person_id variablen
+         * check om bruger allerede findes
+         */
         stmt = con.createStatement();
         sql = "SELECT Person_id FROM bruger WHERE Person_id = " + Person_id;
         ResultSet rs = stmt.executeQuery(sql);
@@ -25,6 +35,9 @@ public class Bruger {
         if (hentetId == Person_id) {
             System.out.println("Bruger med Person_id: " + Person_id + " findes allerede!");
         }
+        /**
+         * ellers opretter den bruger som er lavet nede under
+         */
         else {
             System.out.println("Creating statement...");
             stmt = con.createStatement();
@@ -37,7 +50,10 @@ public class Bruger {
         }
     }
 
-    // Metode til at lave flere forskelige brugere med lavBruger metoden
+    /** Metode til at lave flere forskelige brugere med lavBruger metoden
+     *
+     * @throws SQLException søger for hvis der fejl at den bare forsætter
+     */
     public static void insertBrugerData() throws SQLException {
 
         lavBruger( 1,"Erik", "Ruhmanis", "Troensevej");
