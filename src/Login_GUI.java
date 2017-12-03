a7de5854ecaa452bfa44e4006381243cd2c8ffc3
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-12-03 18:06:14 +0100

    Tilføjet alt muligt  lækkert

--- src/Login_GUI.java
+++ src/Login_GUI.java
@@ -33,7 +33,7 @@ public class Login_GUI {
         //Creates the frame/window
         frame = new JFrame("Login Window");
         frame.setBounds(500, 500, 450, 300);
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        //frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE); //Gør at programmet stopper med at køre på X - knappen
         frame.getContentPane().setLayout(null);
 
         //Create user field
